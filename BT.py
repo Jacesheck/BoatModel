eeb15aa651ed6b51d72428eaa5c9dca80abedb60
Jace <jacedenny@gmail.com>
2023-05-17 16:15:26 +1000

    Adjust bluetooth for sending floats

--- BT.py
+++ BT.py
@@ -4,9 +4,24 @@ from bleak import BleakClient, BleakScanner
 import msvcrt
 
 VAL_UUID = "0000fff1-0000-1000-8000-00805f9b34fb"
+COORDS_UUID = "45c1eda2-4473-42a3-8143-dc79c30a64bf"
 
-enter = asyncio.Lock()
-running = True
+async def print_details(address):
+    async with BleakClient(address) as client:
+        services = client.services
+        for s in services:
+            print("Service:", s)
+            print("Type:", type(s))
+            print("Characteristics", s.characteristics)
+            for c in s.characteristics:
+                print('UUID', c.uuid)
+                try:
+                    val = await client.read_gatt_char(c.uuid)
+                    print('len', len(val))
+                    print(struct.unpack('<f', val)) # Little endian
+                except Exception as e:
+                    print(e)
+            print()
 
 def print_val(_, val):
     print(len(val))
@@ -14,37 +29,47 @@ def print_val(_, val):
     print('y', struct.unpack('<f', val[4:8]))
     print('z', struct.unpack('<f', val[8:]))
 
-async def connect(address):
-    async with BleakClient(address) as client:
-        await client.start_notify(VAL_UUID, print_val)
-        while True:
-            await enter.acquire()
-            if running == False:
-                break
-            enter.release()
-            await asyncio.sleep(0.1)
-        await client.stop_notify(VAL_UUID);
-        # TODO: Write to file
+class Central:
+    def __init__(self, address):
+        self.address = address
+    async def connect(self):
+        self.client = BleakClient(self.address)
+        await self.client.connect()
+    async def disconnect(self):
+        await self.client.disconnect()
+    async def start_notify(self):
+        await self.client.start_notify(VAL_UUID, print_val)
+    async def stop_notify(self):
+        await self.client.stop_notify(VAL_UUID);
+    async def write(self, data):
+        await self.client.write_gatt_char(COORDS_UUID, data, True)
 
-async def press_enter():
-    global running
+async def runDevice(address):
+    coords = [8.8, 7.94, 13.782, 23.1217]
+    byteCoords = b''
+    for num in coords:
+        newByte = struct.pack('<f',num)
+        byteCoords += newByte
+        print('Size:', len(newByte))
+    print('Will send')
+    print(byteCoords)
+    central = Central(address)
+    await central.connect()
+    await central.start_notify()
     print('Waiting for enter')
-    while True:
+    while not msvcrt.kbhit():
         await asyncio.sleep(0.1)
-        if msvcrt.kbhit():
-            break
-    print('Pressed enter')
-    await enter.acquire()
-    running = False
-    enter.release()
+    msvcrt.getch()
+    await central.write(byteCoords)
+    while not msvcrt.kbhit():
+        await asyncio.sleep(0.1)
+    await central.stop_notify()
+
 
 async def main():
-    devices = await BleakScanner.discover()
+    devices = await BleakScanner.discover(1.)
     for d in devices:
         if d.name == 'Test':
-            t1 = asyncio.create_task(connect(d.address))
-            t2 = asyncio.create_task(press_enter())
-            await t1
-            await t2
+            await runDevice(d.address)
 
 asyncio.run(main())
