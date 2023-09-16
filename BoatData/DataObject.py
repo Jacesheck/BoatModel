1cace9358a962c50f10b474801c5eab18d3fac86
Jace <jacedenny@gmail.com>
2023-09-16 12:36:16 +1000

    Better unpacking

--- BoatData/DataObject.py
+++ BoatData/DataObject.py
@@ -31,21 +31,15 @@ class DataObject:
             # TODO: Make this shorter
             self.decoded_data['timestamp'].append(point[0])
             telemetry = point[1]
-            i = 0
-            self.decoded_data['gpsX'].append(struct.unpack('<d', telemetry[i: i+8])[0])
-            i += 8
-            self.decoded_data['gpsY'].append(struct.unpack('<d', telemetry[i: i+8])[0])
-            i += 8
-            self.decoded_data['gpsLat'].append(struct.unpack('<d', telemetry[i: i+8])[0])
-            i += 8
-            self.decoded_data['gpsLng'].append(struct.unpack('<d', telemetry[i: i+8])[0])
-            i += 8
-            self.decoded_data['power1'].append(struct.unpack('<l', telemetry[i: i+4])[0])
-            i += 4
-            self.decoded_data['power2'].append(struct.unpack('<l', telemetry[i: i+4])[0])
-            i += 4
-            self.decoded_data['rz'].append(struct.unpack('<f', telemetry[i: i+4])[0])
-            i += 4
+
+            decoded_data = struct.unpack('<ddddllf', telemetry[:44])
+            self.decoded_data['gpsX'].append(decoded_data[0])
+            self.decoded_data['gpsY'].append(decoded_data[1])
+            self.decoded_data['gpsLat'].append(decoded_data[2])
+            self.decoded_data['gpsLng'].append(decoded_data[3])
+            self.decoded_data['power1'].append(decoded_data[4])
+            self.decoded_data['power2'].append(decoded_data[5])
+            self.decoded_data['rz'].append(decoded_data[6])
 
             # Normalise motor powers
             self.decoded_data['power1'][-1] = -IDLE_POWER + self.decoded_data['power1'][-1]
